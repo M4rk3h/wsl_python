584225234718faa768c41e3f1e6302c4bcec45a5
M4rk3h <Kadaj_x@msn.com>
2019-05-17 14:27:48 +0100

    Finished the calculator within Python3

---
+++ calculator.py
@@ -0,0 +1,61 @@
+#	This is going to be a Calculator which was
+#	created by following a guide from DigitalOcean.
+#	https://www.digitalocean.com/community/tutorials/how-to-make-a-simple-calculator-program-in-python-3
+
+#	Define a welcome function
+def welcome():
+	print('''
+Welcome to Python3-Calculator''')
+
+#	Define calculator function
+def calculate():
+    operation = input('''
+Please type in the math operation you would like to complete:
++ for addition
+- for subtraction
+* for multiplication
+/ for division
+
+''')
+
+#	Ask for input
+    number_1 = int(input('Please enter the first number: '))
+    number_2 = int(input('Please enter the second number: '))
+
+    if operation == '+':
+        print('{} + {} = '.format(number_1, number_2))
+        print(number_1 + number_2)
+
+    elif operation == '-':
+        print('{} - {} = '.format(number_1, number_2))
+        print(number_1 - number_2)
+
+    elif operation == '*':
+        print('{} * {} = '.format(number_1, number_2))
+        print(number_1 * number_2)
+
+    elif operation == '/':
+        print('{} / {} = '.format(number_1, number_2))
+        print(number_1 / number_2)
+
+    else:
+        print('You have not typed a valid operator, please run the program again.')
+
+#	Add again() function to calculate() function
+    again()
+#	Define a function to calculate again
+def again():
+    calc_again = input('''
+Do you want to calculate again?
+Please type Y for YES or N for NO.
+''')
+
+    if calc_again.upper() == 'Y':
+        calculate()
+    elif calc_again.upper() == 'N':
+        print('See you later.')
+    else:
+        again()
+
+welcome()
+calculate()
